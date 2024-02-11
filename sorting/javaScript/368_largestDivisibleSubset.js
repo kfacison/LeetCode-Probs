eacb2a938e85b41fc86d35e2acada5d0913e2803
kfac <kfacison1998@gmail.com>
2024-02-10 21:54:57 -0600

    testing 368

--- sorting/javaScript/368_largestDivisibleSubset.js
+++ sorting/javaScript/368_largestDivisibleSubset.js
@@ -23,6 +23,33 @@ All the integers in nums are unique.
 */
 
 function largestDivisibleSubset(nums){
+    nums.sort((a, b) => a - b);
+    const n = nums.length;
+    const groupSize = new Array(n).fill(1);
+    const prevElement = new Array(n).fill(-1);
+    let maxIndex = 0;
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[i] % nums[j] === 0 && groupSize[i] < groupSize[j] + 1) {
+                groupSize[i] = groupSize[j] + 1;
+                prevElement[i] = j;
+            }
+        }
+        if (groupSize[i] > groupSize[maxIndex]) {
+            maxIndex = i;
+        }
+    }
+
+    const result = [];
+    for (let i = maxIndex; i !== -1; i = prevElement[i]) {
+        result.unshift(nums[i]);
+    }
+
+    return result;
+}
+
+function largestDivisibleSubset0(nums){
     const answer = [];
     for(let i=0;i<nums.length;i++){
         for(let j=i+1;j<nums.length;j++){
